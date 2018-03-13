03f00343cd8b89814067b1ecec9a5353c412fe55
El Mahraoui Omar (100) <omar.elmahraoui@cmc.be>
2018-03-13 10:28:49 +0100

    Created jar with dependencies and executed the total program

--- cli/src/main/java/com/switchfully/cli/MyCLI.java
+++ cli/src/main/java/com/switchfully/cli/MyCLI.java
@@ -1,7 +1,18 @@
 package com.switchfully.cli;
 
+import com.switchfully.api.StockExchangeController;
+import com.switchfully.domain.StockRepository;
+import com.switchfully.interfaces.ExternalStockInformationService;
+import com.switchfully.service.StockService;
+
 public class MyCLI {
 
-
+    public static void main(String[] args) {
+        if (args.length == 1) {
+            StockExchangeController stockExchangeController = new StockExchangeController(new StockService(StockRepository.getInstance()
+                    , new ExternalStockInformationService()));
+            System.out.println(stockExchangeController.getStock(args[0]));
+        }
+    }
 
 }
